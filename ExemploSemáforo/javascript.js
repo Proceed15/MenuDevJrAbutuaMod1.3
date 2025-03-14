62fb65d0344a5a17c923eebb02732087f2da15b4
j <@gmail.com>
2025-03-13 22:02:47 -0300

    Exemplo Semáforo Concluído

--- ExemploSemáforo/javascript.js
+++ ExemploSemáforo/javascript.js
@@ -1,9 +1,4 @@
 function changeTrafficLight(){
-    console.log("Hello Javascript!")
-    console.log(document.getElementById("red-light"))
-    console.log(document.getElementById("yellow-light"))
-    console.log(document.getElementById("green-light"))
-
     //If's e Elses
     if (document.getElementById("yellow-light").className==="yellow-light yellow-light"){
         document.getElementById("red-light").className="red-light red-light";
