ecb77c98d4c82a3cb781eeeb68c382ac6e3da439
j <@gmail.com>
2025-03-16 22:10:08 -0300

    Solução Oper Concluída

--- CalculadoraProjetoJavascriptHTMLCSS/javascript.js
+++ CalculadoraProjetoJavascriptHTMLCSS/javascript.js
@@ -2,11 +2,10 @@ function calc(oper){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
 
-    var sum = document.getElementById("sum");
-    var sui = document.getElementById("sui");
-    var multi = document.getElementById("multi");
-    var divid = document.getElementById("divid");
-
+    //With oper as the parameter it gets the the id of each button
+    //with the property this and returns the id for the button pressed
+    //the if's them compare the id received in the oper for the current
+    //operation and performs the task, it can be done in sequence by each
     if(oper.id == "sum"){
         var result = num1 + num2;
     }
@@ -19,6 +18,16 @@ function calc(oper){
     else if(oper.id == "divid"){
         var result = num1 / num2;
     }
+    //For parameters such as 'sum', 'sui', 'multi' and 'divid' 
+    //in the button use the switch case below
+    /*
+    switch(op){
+        case 'sum': result = num1 + num2; break;
+        case 'sui': result = num1 + num2; break;
+        case 'multi': result = num1 + num2; break;
+        case 'divid': result = num1 + num2; break;
+    }
+    */
     document.getElementById("result").value = result;
 
 }
