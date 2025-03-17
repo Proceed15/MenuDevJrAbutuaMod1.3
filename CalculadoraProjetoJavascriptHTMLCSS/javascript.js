86fbf5853e0428c587e4db5a3557ac7f7ce28704
j <@gmail.com>
2025-03-16 21:51:35 -0300

    Funcionalidades Oper

--- CalculadoraProjetoJavascriptHTMLCSS/javascript.js
+++ CalculadoraProjetoJavascriptHTMLCSS/javascript.js
@@ -1,8 +1,24 @@
-function calc(){
+function calc(oper){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
-    var result = num1 + num2;
 
+    var sum = document.getElementById("sum");
+    var sui = document.getElementById("sui");
+    var multi = document.getElementById("multi");
+    var divid = document.getElementById("divid");
+
+    if(oper.id == "sum"){
+        var result = num1 + num2;
+    }
+    else if(oper.id == "sui"){
+        var result = num1 - num2;
+    }
+    else if(oper.id == "multi"){
+        var result = num1 * num2;
+    }
+    else if(oper.id == "divid"){
+        var result = num1 / num2;
+    }
     document.getElementById("result").value = result;
 
 }
